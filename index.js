c47408261be93b2e76e2f0400cdccd7468a80a73
Michael Kroth <mkroth65@gmail.com>
2018-02-21 16:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object
 }
-function 
-return Object.assign({}, obj, { [key]: value })
+// function 
+// return Object.assign({}, obj, { [key]: value })
